62811f4ab679e4114f896a630f4bf9cab60a8897
Himanshu Singh <75830024+singhhimanshu31@users.noreply.github.com>
2022-10-25 17:54:23 +0530

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,19 +4,7 @@ function Nav(props) {
     <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
         <div className="container-fluid">
         <a className="navbar-brand">{props.branding}</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar" >
-            <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="collapsibleNavbar">
-            <ul className="navbar-nav">
-            <li className="nav-item">
-                <a className="nav-link" href='/'>Home</a>
-            </li>
-            <li className="nav-item">
-                <a className="nav-link" href='/about'>About</a>
-            </li>
-            </ul>
-        </div>
+        
         </div>
     </nav>
     );
